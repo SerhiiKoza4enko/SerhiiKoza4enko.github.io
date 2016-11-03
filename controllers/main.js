69656650a1e1018e5be3440f07d9e79b94e534ae
Sergey Kozachenko <sergey.kozachenko@intellimedia.ca>
2016-11-03 23:01:54 +0200

    Commit #2

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('SiteModule', ["firebase"]).controller('MainController', function($scope, $http, $timeout, $firebase) {
+angular.module('SiteModule', ["firebase", "ui.bootstrap", "ngAside"]).controller('MainController', function($scope, $http, $timeout, $firebase, $uibModal, $sce) {
     $scope.works = [];
     var ref = new Firebase("https://prikol-be70b.firebaseio.com");
     var obj = $firebase(ref).$asObject();
@@ -11,6 +11,19 @@ angular.module('SiteModule', ["firebase"]).controller('MainController', function
     $scope.url = "";
 
     $scope.showDoc = function(file) {
-        console.log(file);
+        $scope.url = $sce.trustAsResourceUrl("https://docs.google.com/gview?url=https://serhiikoza4enko.github.io/resources/documents/" + file.name + "&embedded=true");
+        $scope.title = file.title;
+        $scope.modalInstance = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: './partials/doc-view.html',
+            windowClass: 'docviewer',
+            scope:$scope
+        });
+    }
+
+    $scope.close = function() {
+        $scope.modalInstance.dismiss();
     }
 });
