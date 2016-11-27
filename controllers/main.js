be3d69ed319587ffafb831b6a56abc2680df381b
Sergey Kozachenko <sergey.kozachenko@intellimedia.ca>
2016-11-27 22:09:14 +0200

    Almost last commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,12 +1,18 @@
 'use strict';
 
 angular.module('SiteModule', ["firebase", "ui.bootstrap", "ngAside"]).controller('MainController', function($scope, $http, $timeout, $firebase, $uibModal, $sce) {
+    $scope.active = 0;
     $scope.works = [];
-    var ref = new Firebase("https://prikol-be70b.firebaseio.com");
+    var ref = new Firebase("https://prikol-be70b.firebaseio.com/works");
     var obj = $firebase(ref).$asObject();
     $scope.works = obj;
-
     obj.$bindTo($scope, "works");
+
+
+    ref = new Firebase("https://prikol-be70b.firebaseio.com/photos");
+    obj = $firebase(ref).$asObject();
+    $scope.photos = obj;
+    obj.$bindTo($scope, "photos");
 
     $scope.url = "";
 
@@ -19,11 +25,27 @@ angular.module('SiteModule', ["firebase", "ui.bootstrap", "ngAside"]).controller
             ariaDescribedBy: 'modal-body',
             templateUrl: './partials/doc-view.html',
             windowClass: 'docviewer',
-            scope:$scope
+            scope: $scope
         });
     }
 
     $scope.close = function() {
         $scope.modalInstance.dismiss();
     }
+
+    $scope.open = function(indx) {
+        $scope.active = indx;
+        $scope.photos[indx].active = true;
+
+        $scope.modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'template/pic-modal.html',
+            windowClass: 'photoviewer',
+            scope: $scope
+        });
+    };
+
+    $scope.ok = function() {
+        $scope.modalInstance.close();
+    };
 });
